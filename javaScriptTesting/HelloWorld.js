a5e7ea19f855d537af2398d71403808ff87ff93a
benjaminLeongSK <benjaminlsk@live.com>
2022-11-18 10:53:08 +0800

    test

---
+++ javaScriptTesting/HelloWorld.js
@@ -0,0 +1,31 @@
+console.log("Hello World!");
+
+function sum(a,b){
+    return a+b;
+};
+
+console.log(sum(5,5))
+
+function sub(a,b){
+    return a-b;
+};
+
+console.log(sub(5,5));
+
+function div(a,b){
+    return a/b;
+};
+
+console.log(div(5,5));
+
+function mul(a,b){
+    return a*b;
+};
+
+console.log(mul(5,5));
+
+function Hello(name){
+    return console.log("Hello"+ name);
+};
+
+Hello(" benjamin yo wassup asdasdsdasd");
