054c26e47577e3c2adf19d3744e8306712b00ac9
benjaminLeongSK <benjaminlsk@live.com>
2023-01-28 10:35:14 +0800

    add classcomponent example

---
+++ module6/reactEX/ClassComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+    render () {
+
+        return <h2> I am the Class Component</h2>;
+
+    }
+}
+
+export default ClassComponent;
--- module6/reactEX/index.js
+++ module6/reactEX/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ClassComponent from "./ClassComponents";
 
 function App() {
     return (
 
         <>
             <h1>My React Example</h1>
+            <ClassComponent />
         </>
+
 
     )
 }
