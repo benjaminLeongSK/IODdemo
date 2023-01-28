55ff4d712be77bfaef96cda32c9706c42bc264cf
benjaminLeongSK <benjaminlsk@live.com>
2023-01-28 10:26:28 +0800

    initial commit

---
+++ module6/reactEX/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+
+        <>
+            <h1>My React Example</h1>
+        </>
+
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
