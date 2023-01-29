8bef264511d5a7851b3d68a91db2ff739cf092d4
benjaminLeongSK <benjaminlsk@live.com>
2023-01-29 22:19:04 +0800

    added bootstrap and json into html

--- module4/mod4lab/index.js
+++ module4/mod4lab/index.js
@@ -1,13 +1,32 @@
-function addCard() {
 
-    const template =
-        document.getElementById("card-template").content.cloneNode(true);
-        template.querySelector('.card-title').innerText = 'My Card Title';
-        template.querySelector('.card-text').innerText = 'lorem ipsum ble bla';
-        document.querySelector('#card-list').appendChild(template);
-    }
+    fetch('people.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
 
-    if ('content' in document.createElement('template')) {
-        addCard();
-        addCard();
+function appendData(data) {
+    var mainContainer = document.getElementById("myRanking");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = data[i].id;
+        mainContainer.appendChild(div);
     }
+    var mainContainerData = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = data[i].firstName + ' ' + data[i].lastName;
+        mainContainerData.appendChild(div);
+    }
+    var mainContainerPoints = document.getElementById("myPoints");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = data[i].points;
+        mainContainerPoints.appendChild(div);
+    }
+}
