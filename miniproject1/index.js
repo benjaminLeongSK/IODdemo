36dcfa769161a18b81b41a48ad06d6f353903179
benjaminLeongSK <benjaminlsk@live.com>
2023-02-03 10:51:52 +0800

    add bgimage and corrected bug

--- miniproject1/index.js
+++ miniproject1/index.js
@@ -1,13 +1,11 @@
 
 
-fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=ccbe0d402e72c29b0e7ad5bb3a66a4b4&hash=4b8e06ffd8bd5b31dd33d93b94892036&ts=1675259668304&limit=10')
+fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=ccbe0d402e72c29b0e7ad5bb3a66a4b4&hash=4b8e06ffd8bd5b31dd33d93b94892036&ts=1675259668304&limit=12')
 .then(function (response) {
     return response.json();
 })
 .then(function (data) {
     addcard(data);
-    // appendData(data);
-    // console.log(data.data.results[0].name);
 })
 .catch(function (err) {
     console.log('error: ' + err);
@@ -16,10 +14,10 @@ fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=ccbe0d402e72c2
 
 function addcard(data) {
     for (var i = 0; i < data.data.results.length; i++) {
-    const template = document.getElementById("marvel-card").content.cloneNode(true);
-    template.querySelector('#char-img').src=data.data.results[i].thumbnail.path+".jpg"
-    template.querySelector('.char-name').innerText="NAME: " + data.data.results[i].name;
-    template.querySelector('.char-des').innerText="DESCRIPTION: " + data.data.results[i].description;
-    document.querySelector('#card-list').appendChild(template);
+        const template = document.getElementById("marvel-card").content.cloneNode(true);
+        template.querySelector('#char-img').src=data.data.results[i].thumbnail.path+ "." + data.data.results[i].thumbnail.extension
+        template.querySelector('.char-name').innerText="NAME: " + data.data.results[i].name;
+        template.querySelector('.char-des').innerText="DESCRIPTION: " + data.data.results[i].description;
+        document.querySelector('#card-list').appendChild(template);
     }
 }
